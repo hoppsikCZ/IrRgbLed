784e67fa29e3385da6607ad3e5a81c1800a1afd6
hoppsikCZ <120632594+hoppsikCZ@users.noreply.github.com>
2024-05-20 10:20:23 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,100 @@
 #include <Arduino.h>
-#include <IRremote.h>
-#define IR_RECEIVE_PIN 12
-// put function declarations here:
-int myFunction(int, int);
+#define IR_USE_AVR_TIMER1
+#include <IRRemote.hpp>
 
-void setup() {
-   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
-  Serial.begin(9600);
+#define IR_RECEIVE_PIN 2
+#define RED 3
+#define GREEN 5 
+#define BLUE 6
+#define REDCODESKOLA 3091726080 //CH+
+#define GREENCODESKOLA 3158572800 //>||
+#define BLUECODESKOLA 4127850240 //EQ
+#define BACKSKOLA 4161273600 //-
+#define RESETSKOLA 3927310080 //+
 
-}
+unsigned long codesSkola[] =
+    {
+        3910598400,                               // 0
+        4077715200,                               // 1
+        3877175040,                               // 2
+        2707357440,                               // 3
+        4144561920,                               // 4
+        3810328320,                               // 5
+        2774204160,                               // 6
+        3175284480,                               // 7
+        2907897600,                               // 8
+        3041591040                                // 9
+}; 
 
-void loop() {
-  if(IrReceiver.decode()){
-    unsigned long irData = IrReceiver.decodedIRData.decodedRawData;
-    if(irData)
+//Funkce pro IR vstup
+void IrInput() {
+  //Inicializace promennych potrebnych pro fukci
+  bool next = true;
+  int number = 0;
+  
+  while (next)
+  {
+    //Pokus o precteni IR senzoru
+    if (IrReceiver.decode()) 
+    {
+      //Vypis informaci na seriovou linku a nacteni dat do promenne
+      IrReceiver.printIRResultShort(&Serial);
+      unsigned long code = IrReceiver.decodedIRData.decodedRawData;
+      Serial.println(code, DEC);
+      
+      //Kontrola jestli je vstupni kod cislo
+      for (size_t i = 0; i < 10; i++)
+      {
+        if (code == codesSkola[i]) {
+          number *= 10;
+          number += i;
+        }
+      }
+      
+      //Kontrola pro dalsi kody
+      switch (code)
+      {
+        //Smazani posledni cifry
+        case BACKSKOLA:
+          number /= 10;
+          break;
+        //Reset celeho cisla pomoci ukonceni funkce
+        case RESETSKOLA:
+          next = false;
+          break;
+        //Rozsviceni jednotlivych ledek
+        case REDCODESKOLA:
+          analogWrite(RED, number > 255 ? 255 : number);
+          break;
+        case GREENCODESKOLA:
+          analogWrite(GREEN, number > 255 ? 255 : number);
+          break;
+        case BLUECODESKOLA:
+          analogWrite(BLUE, number > 255 ? 255 : number);
+          break;
+        default:
+          break;
+      }
+      //Prikaz k pokracovani monitorovani IR prijimace
+      IrReceiver.resume();
+    } 
   }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void setup() {
+  //Priprava pinu a zapnuti seriove linky
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //Testuje se jestli prisel nejaky IR vstup
+  if (IrReceiver.available())
+  {
+    IrInput();
+  }
 }
